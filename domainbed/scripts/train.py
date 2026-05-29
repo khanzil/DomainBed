913afe5cb3b5e6a3c36d16d936b3914c7ca07146
Lương Tiến Khanh <128344602+khanzil@users.noreply.github.com>
2026-05-29 14:14:58 +0700

    .

--- domainbed/scripts/train.py
+++ domainbed/scripts/train.py
@@ -214,13 +214,14 @@ if __name__ == "__main__":
                 for x,_ in next(uda_minibatches_iterator)]
         else:
             uda_device = None
+        temp_gb = torch.cuda.max_memory_allocated() / (1024.*1024.*1024.)
+        print(temp_gb)    
         step_vals = algorithm.update(minibatches_device, uda_device)
         checkpoint_vals['step_time'].append(time.time() - step_start_time)
 
         for key, val in step_vals.items():
             checkpoint_vals[key].append(val)
-        temp_gb = torch.cuda.max_memory_allocated() / (1024.*1024.*1024.)
-        print(temp_gb)
+
         if (step % checkpoint_freq == 0) or (step == n_steps - 1):
             results = {
                 'step': step,
